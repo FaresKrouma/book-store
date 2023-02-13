cad9766b1b906994c926e4028bd3987e8b45bf7d
Fares Krouma <88767998+FaresKrouma@users.noreply.github.com>
2023-02-13 22:24:23 +0100

    created client side react-app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
