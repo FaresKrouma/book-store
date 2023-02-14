d6d39d7fdbdef13af0522ff8b46062f9ae9d9026
Fares Krouma <88767998+FaresKrouma@users.noreply.github.com>
2023-02-14 22:11:15 +0100

    added functionality to components(add,book)

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
-  return <div>Add</div>;
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    cover: "",
+    price: "",
+  });
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3000/books", book);
+      // navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(book);
+  return (
+    <form>
+      <h2>add new book</h2>
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="title"
+        name="title"
+      />
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="description"
+        name="desc"
+      />
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="price"
+        name="price"
+      />
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="cover"
+        name="cover"
+      />
+      <button onClick={handleSubmit}>submit book</button>
+    </form>
+  );
 };
 
 export default Add;
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
@@ -8,7 +9,7 @@ const Books = () => {
     const fetchAllBooks = async () => {
       try {
         const res = await axios.get("http://localhost:3000/books");
-        setBooks(res);
+        setBooks(res.data);
       } catch (err) {
         console.log(err);
       }
@@ -17,7 +18,26 @@ const Books = () => {
     fetchAllBooks();
   }, []);
 
-  return <div></div>;
+  return (
+    <>
+      <h1>Fares Book Store.</h1>
+      <div className="books-container">
+        {books.map((book) => {
+          return (
+            <div className="book" key={book.id}>
+              <h3> {book.title}</h3>
+              <p>{book.desc}</p>
+              {book.cover && <img src={book.cover} alt="" />}
+              <span>{book.price}</span>
+            </div>
+          );
+        })}
+        <Link to="/add">
+          <button> Add a new book</button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Books;
